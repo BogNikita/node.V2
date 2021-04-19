320540ac7b4487776df364b9a4f6abfadfe888ca
Nikita <nik-bogachev@mail.ru>
2021-04-19 13:30:46 +0500

    first commit

--- 01/index.js
+++ 01/index.js
@@ -1,27 +1,33 @@
 const colors = require('colors/safe');
 const arrayColors = ['green', 'yellow', 'red'];
 
-function isNatural(num) {
-  const check = [];
-  if (num > 3 && num !== 5) {
-    for (let i = 2; i <= 5; i++) {
-      if (i === 4) {
-        continue;
+function isPrime(num) {
+  if (num > 1) {
+    for (let i = 2, max = num ** 0.5; i <= max; i++) {
+      if (!(num % i)) {
+        return false;
       }
-      const result = num % i;
-      check.push(result);
     }
   }
-  const test = check.every((item) => item !== 0);
-  return test;
+  return num > 1;
 }
 
-function start(start, end) {
-  if (Number(start) && Number(end)) {
+function getPrimesInterval(a, b) {
+  let start = Number(a);
+  let end = Number(b);
+
+  if (start && end) {
+    if (start > end) {
+      start = start + end;
+      end = start - end;
+      start = start - end;
+    }
+
     let trafficLight = 0;
     let flag = false;
+
     for (let i = start; i <= end; i++) {
-      if (isNatural(i)) {
+      if (isPrime(i)) {
         flag = true;
         console.log(colors[arrayColors[trafficLight]](i));
         trafficLight++;
@@ -33,9 +39,11 @@ function start(start, end) {
     if (!flag) {
       console.log(colors.red('В диапозоне нет натуральных чисел'));
     }
+    return;
   } else {
-      console.log('error');
+    console.error('Вы ввели не число');
+    return;
   }
 }
 
-start(1, false);
+getPrimesInterval(process.argv[2], process.argv[3]);
